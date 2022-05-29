3b2051c185f66a553d262165de2e97b2cea3d644
jaeyong <jaeyong@jaeyongui-MacBookAir.local>
2022-05-29 16:50:01 +0900

    [MODIFY]UI 수정

--- app/src/androidTest/java/com/example/cnu_graduation_project/DrivingRecognitionActivityTest.java
+++ app/src/androidTest/java/com/example/cnu_graduation_project/DrivingRecognitionActivityTest.java
@@ -1,4 +1,46 @@
+package com.example.cnu_graduation_project;
+
+import static org.junit.Assert.assertEquals;
+
+import android.util.Log;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.google.android.gms.location.DetectedActivity;
+
 import junit.framework.TestCase;
-public class DrivingRecognitionActivityTest extends TestCase {
-  
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class DrivingRecognitionActivityTest {
+//    @Rule
+    public ActivityScenarioRule rule;
+    @Before
+    public void setUp(){
+        rule = new ActivityScenarioRule<>(DrivingRecognitionActivity.class);
+
+    }
+    @Rule
+    public ActivityScenarioRule<DrivingRecognitionActivity> mActivityRule =
+            new ActivityScenarioRule(DrivingRecognitionActivity.class);
+    @Test
+    public void myTest() throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+
+        int expected = 3;
+
+        Method method = DrivingRecognitionActivity.class.getDeclaredMethod("toActivityString", int.class);
+        method.setAccessible(true);
+        int actual = (int) method.invoke(mActivityRule.getScenario(), DetectedActivity.IN_VEHICLE);
+
+        assertEquals(expected, actual);
+    }
 }
